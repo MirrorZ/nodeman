1fb3e1371f2d52d28f9a516facc61f3dff9efc3e
ashmew2 <money_man_91@yahoo.com>
2014-12-18 16:35:08 +0530

    Added setup script for node. Use it from now

---
+++ setup_node.sh
@@ -0,0 +1,19 @@
+# Sets up a new mesh interface on a supported wlan interface.
+# USAGE : # ./setup_gate wlanX meshY 
+#
+
+#!/bin/bash
+
+if [ $# -ne 2 ]; then
+	echo "Usage : # ./setup_gate wlanX meshY"
+	exit 1
+fi
+
+echo "Adding interface mesh0"
+
+iw dev $1 interface add $2 type mp
+ifconfig $2 up
+iw dev $2 mesh join openmesh
+
+echo "Done."
+
