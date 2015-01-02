3ce0de6d44a9e52371aee3043820af1114b832a9
root <sudip1401@gmail.com>
2015-01-03 01:28:17 +0530

    Looked at each ping taking into consideration unknown and unreachable host. The last method was crap. This one is more robust.

--- icmpping.sh
+++ icmpping.sh
@@ -16,14 +16,27 @@ last_recorded_state=0
 
 for i in ${servers_to_ping[*]}
 do
-	output=$(ping -c $ping_count ${i})	
-        packetloss=$(echo "${output}" | awk '/packet loss/ {print $6} ')
-        last_ping_state=$(echo $output | tail -5 | grep -i -c 'unreachable');
-        last_recorded_state=$(($last_ping_state ^ 1))
-	echo "Packetloss : ${packetloss}"
+	j=0
+	packets_lost=0
+	while [ $j -lt $ping_count ]
+	do
+		output=$(ping -c 1 ${i} 2>&1)
+		packetloss=$(echo "${output}" | awk '/packet loss/ {print $6} ')
+        last_ping_state_0=$(echo $output | grep -i -c 'unreachable');
+        last_ping_state_1=$(echo $output | grep -i -c 'unknown');
+        #echo output=$output
+        last_recorded_state=$(( $(($last_ping_state_0 ^ 1)) & $(($last_ping_state_1 ^ 1)) ))
+        if [ $last_recorded_state -eq 0 ]; then
+        	packets_lost=$((packets_lost + 1))
+        fi
+        #echo $last_ping_state_0, $last_ping_state_1, $last_recorded_state
+        #echo "-----------"
+        j=$((j+1))
+	done
+	echo "Packetloss : ${packets_lost}/${ping_count}"
         avgrtt=$(echo "${output}" | awk '/rtt/ {split($4,a,/\//);print a[2]}')
 	echo "${i}: $avgrtt ms"
-		host_unreachable_count=$(echo $output | grep -c -i 'unreachable');
+		host_unreachable_count=$(echo $output | grep -c 'unreachable');
 	echo "host_unreachable_count: ${host_unreachable_count}"
 done
 
@@ -33,10 +46,9 @@ output=$(speedtest-cli --simple)
 
 #upload
 echo $output | awk '{print $8 " " $9}'
-
 #download
 echo $output | awk '{print $5 " " $6}'
- 
+
 #last recorded network state from ping. 0:down, 1:up
 echo $last_recorded_state
 
