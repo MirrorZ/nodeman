9eb07f0035ecb7c4cbc68e2aa5736f30e42b5def
root <sudip1401@gmail.com>
2015-01-02 23:58:54 +0530

    incorporated the network state in icmpping. If network goes down and comes back up, we get n/w is up.

--- icmpping.sh
+++ icmpping.sh
@@ -9,6 +9,7 @@
 
 servers_to_ping=(www.google.com www.yahoo.com www.nytimes.com)
 ping_count=10
+last_recorded_state=0
 #-----------------------------Latency-----------------------------
 
 #This is a sequential ping to the servers.
@@ -17,6 +18,8 @@ for i in ${servers_to_ping[*]}
 do
 	output=$(ping -c $ping_count ${i})	
         packetloss=$(echo "${output}" | awk '/packet loss/ {print $6} ')
+        last_ping_state=$(echo $output | tail -5 | grep -i -c 'unreachable');
+        last_recorded_state=$(($last_ping_state ^ 1))
 	echo "Packetloss : ${packetloss}"
         avgrtt=$(echo "${output}" | awk '/rtt/ {split($4,a,/\//);print a[2]}')
 	echo "${i}: $avgrtt ms"
@@ -30,7 +33,11 @@ output=$(speedtest-cli --simple)
 
 #upload
 echo $output | awk '{print $8 " " $9}'
+
 #download
 echo $output | awk '{print $5 " " $6}'
  
+#last recorded network state from ping. 0:down, 1:up
+echo $last_recorded_state
+
 exit
