7ae353e63582dd6c41e8a9ea8495233e1df6b405
ashmew2 <ashmew2@gmail.com>
2015-01-20 13:19:38 +0530

    Added IP address configuration to setup_node.sh

--- setup_node.sh
+++ setup_node.sh
@@ -1,20 +1,21 @@
 # Sets up a new mesh interface on a supported wlan interface.
-# USAGE : # ./setup_gate wlanX meshY 
+# USAGE : # ./setup_gate wlanX meshY meshY_IP
 #
 
 #!/bin/bash
 
-if [ $# -ne 2 ]; then
-	echo "Usage : # ./setup_gate wlanX meshY"
+if [ $# -ne 3 ]; then
+	echo "Usage : # ./setup_gate wlanX meshY meshY_IP"
 	exit 1
 fi
 
-echo "Adding interface mesh0"
+echo "Adding interface."
 
 iw dev $1 interface add $2 type mp
 ifconfig $1 down
 ifconfig $2 up
 iw dev $2 mesh join openmesh
+ifconfig $2 $3
 
 echo "Done."
 
