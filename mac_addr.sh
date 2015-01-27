f8b93392730614108807beaf7324768e621e4ca6
rrshah <rutu.shah.26@gmail.com>
2015-01-27 19:44:24 +0530

    Extract usb0 MAC

--- mac_addr.sh
+++ mac_addr.sh
@@ -4,7 +4,9 @@ if [ -z $packet_recv ]; then
 	mac_list=$(arp)
 	mac_addr=$(echo "${mac_list}" | awk '/192.168.42.129/ {print $3} ')
 	echo "${mac_addr}"
-	ebtables -t nat -A PREROUTING -p arp --arp-opcode Request --arp-mac-src ${mac_addr} -j arpreply --arpreply-mac MAC
+	usb_ifconfig=$(ifconfig usb0)
+	usb_mac_addr=$(echo "${usb_ifconfig}" | awk '/HWaddr/ {print $5} ')	
+	ebtables -t nat -A PREROUTING -p arp --arp-opcode Request --arp-mac-src ${mac_addr} -j arpreply --arpreply-mac ${usb_mac_addr}
 else
 	echo "Network Unreachable"
 fi
