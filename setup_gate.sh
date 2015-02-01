47ac867286e794149887c139d7e65c3d47cd9321
Ashish Gupta <ashmew2@gmail.com>
2015-02-01 19:19:20 +0530

    Added click script execution to setup_gate shell script

--- setup_gate.sh
+++ setup_gate.sh
@@ -1,14 +1,17 @@
 #!/bin/bash
 
-# if [ $# -ne 4 ]; then
-#     echo "Usage : # ./setup_gate wlanX meshY <other-bridge-interface> <bridge>"
-#     exit 1
-# fi
+if [ $# -ne 5 ]; then
+     echo "Usage : # ./setup_gate wlanX meshY <other-bridge-interface> <bridge> IPv4_gateway"
+     exit 1
+fi
 
 WLAN_IF=$1
 MESH_IF=$2
 OTHER_BR=$3
 BR_IF=$4
+GW_IP=$5
+TAP_IP=10.0.0.1
+TAP_NW="${TAP_IP}/24"
 
 echo "Setting up the gate."
 iw dev $WLAN_IF interface add $MESH_IF type mp
@@ -33,4 +36,32 @@ ifconfig $MESH_IF up
 
 sleep 2
 iw dev $MESH_IF mesh join openmesh
-echo "Done."
+echo "Done joining openmesh."
+
+sleep 2
+BR_IP=$(ifconfig $BR_IF | grep "inet addr" | awk -F: '{print $2}' | awk '{print $1}')
+
+BR_ETH=$(cat /sys/class/net/$BR_IF/address)
+
+BR_NW="${BR_IP}/24"
+
+
+ip route flush table 0
+
+echo -e "click bridge.click\ 
+			BRIDGE_IF=$BR_IF\
+			BRIDGE_IP=$BR_IP\
+			BRIDGE_MAC=$BR_ETH\
+			BRIDGE_NETWORK=$BR_NW\
+                        GATEWAY_IP=$GW_IP\
+			FAKE_IP=$TAP_IP\			
+			FAKE_NETWORK=$TAP_NW
+"
+
+click bridge.click BRIDGE_IF=$BR_IF\
+		   BRIDGE_IP=$BR_IP\
+		   BRIDGE_MAC=$BR_ETH\
+		   BRIDGE_NETWORK=$BR_NW\
+                   GATEWAY_IP=$GW_IP\
+		   FAKE_IP=$TAP_IP\			
+                   FAKE_NETWORK=$TAP_NW
